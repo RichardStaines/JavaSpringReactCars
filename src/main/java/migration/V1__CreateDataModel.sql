779720cedfea19440cc2c340185447f5d2528b98
Richard Staines <richard.staines@live.com>
2019-04-06 12:26:55 +0100

    move file to proper location for flyway

--- src/main/java/migration/V1__CreateDataModel.sql
+++ src/main/java/migration/V1__CreateDataModel.sql
@@ -1,8 +1,10 @@
---drop table car if exists
---drop table car_owner if exists
---drop table owner if exists
---drop sequence if exists hibernate_sequence
+drop table car if exists;
+drop table car_owner if exists;
+drop table owner if exists;
+drop sequence hibernate_sequence;
+
 create sequence hibernate_sequence start with 1 increment by 1;
+
 create table car (id bigint not null, brand varchar(255), colour varchar(255), model varchar(255), price integer not null, registration_number varchar(255), year integer not null, primary key (id));
 create table car_owner (id bigint not null, owner_id bigint not null, primary key (id, owner_id));
 create table owner (owner_id bigint not null, first_name varchar(255), last_name varchar(255), primary key (owner_id));
@@ -15,3 +17,16 @@ values
 ("John", "Wayne"),
 ("John", "Johnson"),
 ("Harriette", "Hodgson");
+
+insert into car
+(brand, model, colour, registration_number, price, year)
+values
+("Ford", "Mustang", "Red",
+  "ADF-1121", 56000, 1975),
+("MG", "MG3", "Blue",
+  "ZZZ-319", 13000, 2019),
+("Mazda", "6 estate", "Green",
+  "ABC-7072", 34000, 2013);
+
+ insert into car_owner (id, owner_id) values (?, ?)
+
