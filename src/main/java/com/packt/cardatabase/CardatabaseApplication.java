38b209914f96559168aba982d6ea65a98e1d4dbd
Richard Staines <richard.staines@live.com>
2019-04-08 12:48:27 +0100

    Setup initial data. User main application to monitor the sql generated by Hibernate and then move it to the migration file.

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -1,7 +1,10 @@
 package com.packt.cardatabase;
 
+import com.packt.cardatabase.domain.Utils.PasswordEncrypt;
 import com.packt.cardatabase.domain.model.Owner;
+import com.packt.cardatabase.domain.model.User;
 import com.packt.cardatabase.domain.repository.OwnerRepository;
+import com.packt.cardatabase.domain.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -28,6 +31,9 @@ public class CardatabaseApplication {
     @Autowired
     private OwnerRepository ownerRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     private static final Logger logger =
             LoggerFactory.getLogger(CardatabaseApplication.class);
 
@@ -40,7 +46,10 @@ public class CardatabaseApplication {
     CommandLineRunner runner() {
         return args -> {
 
-/* Moved to flyway migration file
+            // never do this in real life i.e. leaving unecrypted password in code.
+     //       this.userRepository.save(new User("admin","admin", "ADMIN" ));
+     //       this.userRepository.save(new User("user", "password123", "USER"));
+/*
             Owner owner1 = new Owner("John", "Johnson");
             Owner owner2 = new Owner("Harriette", "Hodgson");
             ownerRepository.save(owner1);
@@ -53,19 +62,19 @@ public class CardatabaseApplication {
             // save demo data to DB
             carRepository.save(
                     new Car("Ford", "Mustang", "Red",
-                            "ADF-1121", 1975, 56000,
+                            "ADF-1121", "1975", 56000,
                             owner1
                     )
             );
             carRepository.save(
                     new Car("MG", "new-3", "Black",
-                            "ZZZ-999", 2009, 21000,
+                            "ZZZ-999", "2009", 21000,
                             owner2
                     )
             );
             carRepository.save(
                     new Car("Mazda", "6 Estate", "Blue",
-                            "CF99-FAN", 2013, 19000,
+                            "CF99-FAN", "2013", 19000,
                             owner1, owner2
                     )
             );
---
+++ src/main/resources/db/migration/V2__LoadInitialData.sql
@@ -0,0 +1,31 @@
+-- setup initial set of users
+insert into user
+(username, password, role)
+values
+("admin", "$2a$10$FQu86C7r9lm9eBxyC5/MyOiJ7lnb1zmL.8ou1rJ4N1kik.2L0LRAW", "ADMIN"),
+("user", "$2a$10$7vBOxO17g6yOA8liSe8NOOzVIXg1iQ2M/p2Pu.8/CQAtMbiEdr33e", "USER");
+
+-- This is test data really
+insert into owner
+(first_name, last_name)
+values
+("John", "Wayne"),
+("John", "Johnson"),
+("Harriette", "Hodgson");
+
+insert into car
+(brand, model, colour, registration_number, price, year)
+values
+("Ford", "Mustang", "Red",
+  "ADF-1121", 56000, 1975),
+("MG", "MG3", "Blue",
+  "ZZZ-319", 13000, 2019),
+("Mazda", "6 estate", "Green",
+  "ABC-7072", 34000, 2013);
+
+ insert into car_owner
+  (id, owner_id)
+ values
+  (1, 1),
+  (2, 2),
+  (3, 2);
