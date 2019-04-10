6e8e6d11dd61534efab89e16c9f5a8dc5e1b88f8
Richard Staines <richard.staines@live.com>
2019-04-10 11:50:27 +0100

    Side tracked - display data from openWeather RestAPI - wow so easy in React!

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -4,36 +4,45 @@ import './App.css';
 class App extends Component {
   constructor(props) {
       super(props);
-      this.state = {firstName: '', lastName: '', email: ''}
+      this.state = {temp: 0, desc: '', icon: '', loading: true}
   }
 
-  inputChanged = (event) => {
-    this.setState({[event.target.name]: event.target.value});
-  }
 
-  handleSubmit = (event) => {
-    alert(`Hello ${this.state.firstName} ${this.state.lastName}`);
-    event.preventDefault();
-  }
+componentDidMount() {
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&APIkey=b6910acdab0d2b731000bbdea40922c8')
+    .then(response => response.json())
+    .then(responseData => {
+        this.setState(
+            {
+            temp: responseData.main.temp,
+            desc: responseData.weather[0].description,
+            icon: responseData.weather[0].icon,
+            loading: false
+            }
+        )})
+        .catch(err => console.error(err));
+    }
 
   render() {
+    const imgSrc = 'http://openweathermap.org/img/w/' +
+                    this.state.icon + '.png';
+
+    if (this.state.loading) {
+        return <p>Loading...</p>;
+    }
+    else {
     return (
-
-      <form onSubmit={this.handleSubmit} >
-      <label>First Name </label>
-      <input type="text" name="firstName" onChange={this.inputChanged}
-            value={this.state.firstName} /><br />
-      <label>Last Name </label>
-      <input type="text" name="lastName" onChange={this.inputChanged}
-            value={this.state.lastName} /><br />
-      <label>Email </label>
-      <input type="text" name="email" onChange={this.inputChanged}
-            value={this.state.email} onChange={this.inputChanged}/><br />
-      <input type="submit" value="Press me" />
-      </form>
-
+        <div className="App">
+        <p>Temperature: {this.state.temp} &deg;C</p>
+        <p>Description: {this.state.desc}</p>
+        <img src={imgSrc} alt="Weather icon" />
+        </div>
     );
+    }
+
+
   }
+
 }
 
 export default App;
